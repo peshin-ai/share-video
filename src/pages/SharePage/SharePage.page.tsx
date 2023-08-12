6f20dbb95eb3df68d2a14d59d1e04e78fb9c33ef
Tuan Ung <t00aienl@gmail.com>
2023-08-12 10:05:44 +0700

    feat: sharePage

---
+++ src/pages/SharePage/SharePage.page.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export const SharePage = () => {
+  return (
+    <div>
+      <Typography>Share Page</Typography>
+    </div>
+  );
+};
+
+export default SharePage;
