3df38e240ca8b9a3f9dcb2d3c52b313d8497b92c
Tuan Ung <t00aienl@gmail.com>
2023-08-28 23:50:58 +0700

    fix: mockdata to render list

--- src/pages/HomePage/HomePage.page.tsx
+++ src/pages/HomePage/HomePage.page.tsx
@@ -1,17 +1,48 @@
-import { Typography, Box } from "@mui/material";
-import { Header } from "@components/AppHeader";
-import { Helmet } from "react-helmet-async";
+import type { ContentProps } from '@components/Content';
+import type { ReactElement } from 'react';
 
-export const HomePage = () => {
+import { Content } from '@components/Content';
+import {
+  Box,
+  Container,
+  Divider,
+} from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { Helmet } from 'react-helmet-async';
+
+import { mockMovieData } from './mockdata';
+
+export const HomePage = (): ReactElement => {
+  const theme = useTheme();
+
+  const handleLikeItem = (itemId: string) => {
+    console.log(`Like item ${itemId}`);
+  };
+
+  const handleDislikeItem = (itemId: string) => {
+    console.log(`Dislike item ${itemId}`);
+  };
   return (
     <Box>
       <Helmet>
         <title>Funny Movies</title>
         <meta name="description" content="Funny Movies" />
       </Helmet>
-      <form>
-        <Header />
-      </form>
+      <Container maxWidth="lg" sx={{ pt: theme.spacing(3) }}>
+        {mockMovieData.map(
+          (post: ContentProps): ReactElement => (
+            <>
+              <Content
+                key={post.movieId}
+                {...post}
+                onLikesMovie={handleLikeItem}
+                onDislikesMovie={handleDislikeItem}
+              />
+              <Divider sx={{ my: theme.spacing(2) }} />
+            </>
+          )
+        )}
+      </Container>
     </Box>
   );
 };
---
+++ src/pages/HomePage/mockdata.ts
@@ -0,0 +1,28 @@
+export const mockMovieData = [
+  {
+    movieId: 'DF57Ki04vD8?si=4JRh62eBgz0qEMq-',
+    movieTitle: 'Introduction to React Hooks',
+    movieAuthor: 'John Reactson',
+    movieLikes: 120,
+    movieDislikes: 5,
+    movieDescription: 'Learn the basics of React Hooks and how to use them in your projects. We will cover useState, useEffect and custom hooks.',   
+    isUserVoted: false,
+  },
+  {
+    movieId: '2',
+    movieTitle: 'Advanced CSS Techniques',
+    movieAuthor: 'Emily Styler',
+    movieLikes: 95,
+    movieDislikes: 10,
+    movieDescription: 'Explore advanced CSS techniques for creating stunning web designs.',
+    isUserVoted: true,
+  },
+  {
+    movieId: '3',
+    movieTitle: 'JavaScript ES2022 Features',
+    movieAuthor: 'Alex JS',
+    movieLikes: 150,
+    movieDislikes: 3,
+    movieDescription: 'Discover the latest features introduced in JavaScript ES2022.',
+  },
+];
