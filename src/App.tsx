af09433ed8f8c2bf6fc568839e5367357e6fb135
Tuan Ung <t00aienl@gmail.com>
2023-08-28 23:49:02 +0700

    fix: move header outside

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import { HomePage } from "./pages/HomePage";
-import { SharePage } from "./pages/SharePage";
+import { Header } from '@components/AppHeader';
+import { FieldValues } from 'react-hook-form';
+import {
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+import { HomePage } from './pages/HomePage';
+import { SharePage } from './pages/SharePage';
 
 function App() {
+  const handleLogin = (data: FieldValues) => {
+    console.log(data);
+  };
+
+  const handleLogout = () => {
+    console.log("Logout");
+  };
+
+  const user = "guest@gmail.com";
+
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/home" replace />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/share" element={<SharePage />} />
-    </Routes>
+    <>
+      <Header
+        handleLogin={handleLogin}
+        handleLogout={handleLogout}
+        user={user}
+      />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/share" element={<SharePage />} />
+      </Routes>
+    </>
   );
 }
 
