4489f1118becc586caf783ee2b5cdd3b5d2b274a
Tuan Ung <t00aienl@gmail.com>
2023-08-28 23:50:39 +0700

    fix: import Share Form

--- src/pages/SharePage/SharePage.page.tsx
+++ src/pages/SharePage/SharePage.page.tsx
@@ -1,10 +1,43 @@
-import { Typography } from "@mui/material";
+import type { ReactElement } from 'react';
 
-export const SharePage = () => {
+import { Box } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { Helmet } from 'react-helmet-async';
+import {
+  FormProvider,
+  useForm,
+} from 'react-hook-form';
+
+import { ShareForm } from './components/ShareForm';
+
+export const SharePage = (): ReactElement => {
+  const theme = useTheme();
+  const methods = useForm();
+  const { getValues } = methods;
+
+  const handleShare = () => {
+    console.log(getValues());
+  };
+
   return (
-    <div>
-      <Typography>Share Page</Typography>
-    </div>
+    <Box>
+      <Helmet>
+        <title>Share Movies</title>
+        <meta name="description" content="Share Movies" />
+      </Helmet>
+      <Box
+        sx={{ pt: theme.spacing(3) }}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <FormProvider {...methods}>
+          <form>
+            <ShareForm onShare={handleShare} />
+          </form>
+        </FormProvider>
+      </Box>
+    </Box>
   );
 };
 
