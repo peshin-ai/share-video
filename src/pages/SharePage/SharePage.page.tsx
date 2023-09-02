abe8bc338ce13e082305fea20f27fc962c54f034
Tuan Ung <t00aienl@gmail.com>
2023-09-02 14:33:00 +0700

    fix: update Share Page

--- src/pages/SharePage/SharePage.page.tsx
+++ src/pages/SharePage/SharePage.page.tsx
@@ -2,21 +2,38 @@ import type { ReactElement } from 'react';
 
 import { Box } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import { sliceMovieURL } from '@utils/sliceMovieURL/sliceMovieURL';
 import { Helmet } from 'react-helmet-async';
 import {
   FormProvider,
   useForm,
 } from 'react-hook-form';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
+import { useAppDispatch } from '../../hooks/store.hook';
+import { handleShareMovie } from '../../redux/appStore/app.operation';
+import { selectGetUserEmail } from '../../redux/appStore/app.selectors';
+import { shareMovieType } from '../../types';
 import { ShareForm } from './components/ShareForm';
 
 export const SharePage = (): ReactElement => {
   const theme = useTheme();
-  const methods = useForm();
-  const { getValues } = methods;
+  const shareFormMethods = useForm();
+  const appDispatch = useAppDispatch();
+  const userEmail = useSelector(selectGetUserEmail);
+  const { getValues } = shareFormMethods;
+  const navigate = useNavigate();
 
   const handleShare = () => {
-    console.log(getValues());
+    const movieInitialData = {
+      movieAuthor: userEmail,
+      movieId: sliceMovieURL(getValues("movieId")),
+      movieTitle: getValues("movieTitle"),
+      movieDescription: getValues("movieDescription"),
+    };
+    appDispatch(handleShareMovie(movieInitialData as shareMovieType));
+    navigate("/home");
   };
 
   return (
@@ -31,9 +48,9 @@ export const SharePage = (): ReactElement => {
         justifyContent="center"
         alignItems="center"
       >
-        <FormProvider {...methods}>
+        <FormProvider {...shareFormMethods}>
           <form>
-            <ShareForm onShare={handleShare} />
+            <ShareForm onShareMovie={handleShare} />
           </form>
         </FormProvider>
       </Box>
