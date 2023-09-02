9916cf96a793997769c749da9c47869faacbe285
Tuan Ung <t00aienl@gmail.com>
2023-09-02 14:31:00 +0700

    fix: update model

--- src/models/app.model.ts
+++ src/models/app.model.ts
@@ -1,9 +1,76 @@
 import type { CombinedState } from 'redux';
 
+import { AlertColor } from '@mui/material';
+import {
+  PayloadAction,
+  SerializedError,
+} from '@reduxjs/toolkit';
+import { AxiosResponse } from 'axios';
+
+export type Movie = {
+  _id: string;
+  movieId: string;
+  movieTitle: string;
+  movieAuthor: string;
+  movieLikes?: number;
+  movieDislikes?: number;
+  movieDescription?: string;
+  movieLikesBy?: string[];
+  movieDislikesBy?: string[];
+};
+
+export type GetMoviesResponse = {
+  movies: Movie[];
+};
+
 export interface AppState {
-    data: object | null;
+  data: object | null;
+  status: "idle" | "loading" | "failed";
+  previousMovies: Movie[];
+  movies: Movie[];
+  movie: Movie | undefined;
+  accessToken: string | undefined;
+  userEmail: string | undefined;
+  snackbarOpen: boolean;
+  snackbarStatus: AlertColor | undefined;
+  snackbarMessage: string | null;
+}
+
+export interface UserLoginResponse {
+  accessToken: string;
+  userEmail: string;
 }
 
 export type ModuleState = CombinedState<{
-	app: AppState;
+  app: AppState;
 }>;
+
+export type GetMoviesFailureAction = PayloadAction<
+  unknown,
+  string,
+  PayloadMetaAction,
+  SerializedError
+>;
+
+export type UserLoginFailureAction = PayloadAction<
+  unknown,
+  string,
+  PayloadMetaAction,
+  SerializedError
+>;
+
+export type GetMoviesSuccessAction = PayloadAction<
+  AxiosResponse<GetMoviesResponse>
+>;
+
+export type UserLoginSuccessAction = PayloadAction<
+  AxiosResponse<UserLoginResponse>
+>;
+
+export type PayloadMetaAction = {
+  aborted: boolean;
+  arg: void;
+  condition: boolean;
+  requestId: string;
+  requestStatus: "rejected";
+} & ({ rejectedWithValue: true } | { rejectedWithValue: false });
