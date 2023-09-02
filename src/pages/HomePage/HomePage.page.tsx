a326b1180d922512f7ab31a2b0bbbd8ab5e689d4
Tuan Ung <t00aienl@gmail.com>
2023-09-02 14:32:46 +0700

    fix: update Home Page

--- src/pages/HomePage/HomePage.page.tsx
+++ src/pages/HomePage/HomePage.page.tsx
@@ -1,5 +1,4 @@
 import type { ContentProps } from '@components/Content';
-import type { ReactElement } from 'react';
 
 import { Content } from '@components/Content';
 import {
@@ -8,42 +7,98 @@ import {
   Divider,
 } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import {
+  type ReactElement,
+  useEffect,
+  useState,
+} from 'react';
 import { Helmet } from 'react-helmet-async';
+import { useSelector } from 'react-redux';
 
-import { mockMovieData } from './mockdata';
+import { useAppDispatch } from '../../hooks/store.hook';
+import {
+  handleDislikesMovie,
+  handleGetMovies,
+  handleLikesMovie,
+} from '../../redux/appStore/app.operation';
+import {
+  selectGetMovies,
+  selectGetUserEmail,
+} from '../../redux/appStore/app.selectors';
 
 export const HomePage = (): ReactElement => {
   const theme = useTheme();
+  const appDispatch = useAppDispatch();
 
-  const handleLikeItem = (itemId: string) => {
-    console.log(`Like item ${itemId}`);
+  const userEmail = useSelector(selectGetUserEmail);
+
+  const [fistTime, setFirstTime] = useState<boolean>(true);
+
+  const handleLikeItem = (movieId: string) => {
+    const data = {
+      userEmail,
+      movieId,
+    };
+
+    appDispatch(handleLikesMovie(data));
   };
 
-  const handleDislikeItem = (itemId: string) => {
-    console.log(`Dislike item ${itemId}`);
+  const handleDislikeItem = (movieId: string) => {
+    const data = {
+      userEmail,
+      movieId,
+    };
+    appDispatch(handleDislikesMovie(data));
   };
+
+  const movies = useSelector(selectGetMovies);
+  console.log(movies);
+
+  // This will render only once at the first time
+  useEffect(() => {
+    if (fistTime) {
+      appDispatch(handleGetMovies());
+      setFirstTime(false);
+    }
+  }, [fistTime, appDispatch]);
+
+  // This will render every 30 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      appDispatch(handleGetMovies());
+    }, 30000);
+
+    return () => clearTimeout(timer);
+  });
+
   return (
-    <Box>
-      <Helmet>
-        <title>Funny Movies</title>
-        <meta name="description" content="Funny Movies" />
-      </Helmet>
-      <Container maxWidth="lg" sx={{ pt: theme.spacing(3) }}>
-        {mockMovieData.map(
-          (post: ContentProps): ReactElement => (
-            <>
-              <Content
-                key={post.movieId}
-                {...post}
-                onLikesMovie={handleLikeItem}
-                onDislikesMovie={handleDislikeItem}
-              />
-              <Divider sx={{ my: theme.spacing(2) }} />
-            </>
-          )
-        )}
-      </Container>
-    </Box>
+    <Container maxWidth="lg" sx={{ pt: theme.spacing(3) }}>
+      <Box>
+        <Helmet>
+          <title>Funny Movies</title>
+          <meta name="description" content="Funny Movies" />
+        </Helmet>
+
+        <Box width="100%" sx={{ boxShadow: 5, borderRadius: theme.spacing(1) }}>
+          <Box p={theme.spacing(2)}>
+            {movies &&
+              movies.map(
+                (post: ContentProps): ReactElement => (
+                  <>
+                    <Content
+                      key={post.movieId}
+                      {...post}
+                      onLikesMovie={handleLikeItem}
+                      onDislikesMovie={handleDislikeItem}
+                    />
+                    <Divider sx={{ my: theme.spacing(2) }} />
+                  </>
+                )
+              )}
+          </Box>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
