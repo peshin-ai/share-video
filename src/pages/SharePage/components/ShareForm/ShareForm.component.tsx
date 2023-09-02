6d8ecccb629355a5aeb56cfb599b6b39fc359057
Tuan Ung <t00aienl@gmail.com>
2023-09-02 14:32:21 +0700

    fix: update Form Share movie

--- src/pages/SharePage/components/ShareForm/ShareForm.component.tsx
+++ src/pages/SharePage/components/ShareForm/ShareForm.component.tsx
@@ -2,77 +2,139 @@ import { FormTextField } from '@components/Form/FormText/FormText.component';
 import {
   Box,
   Button,
+  Container,
   Grid,
   Typography,
 } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import {
+  useEffect,
+  useState,
+} from 'react';
 import { useFormContext } from 'react-hook-form';
 
 export type ShareFormProps = {
-  onShare: () => void;
+  onShareMovie: () => void;
 };
 
 export const ShareForm = (props: ShareFormProps) => {
-  const { onShare } = props;
-  const { control } = useFormContext();
+  const { onShareMovie } = props;
   const theme = useTheme();
 
+  const { control, watch } = useFormContext();
+
+  const [isCanShare, setIsCanShare] = useState<boolean>(false);
+
+  const [movieId, movieTitle] = watch(["movieId", "movieTitle"]);
+
+  useEffect(() => {
+    if (movieId && movieTitle) {
+      setIsCanShare(true);
+    } else {
+      setIsCanShare(false);
+    }
+  }, [movieId, movieTitle]);
+
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      mt={theme.spacing(20)}
-      border={1}
-      p={theme.spacing(5)}
-      position="relative"
-    >
+    <Container maxWidth="lg">
       <Box
-        position="absolute"
-        bgcolor={theme.palette.common.white}
-        top={0}
-        left={theme.spacing(1)}
-        mt={`-${theme.spacing(2)}`}
-        px={theme.spacing(1)}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        mt={theme.spacing(20)}
+        border={1}
+        p={theme.spacing(5)}
+        position="relative"
       >
-        <Typography variant="h6" gutterBottom>
-          Share a Youtube Movie
-        </Typography>
-      </Box>
-      <Grid container spacing={4} justifyContent="center" alignItems="center">
-        <Grid
-          item
-          container
-          xs={12}
-          justifyContent="center"
-          alignItems="center"
+        <Box
+          position="absolute"
+          bgcolor={theme.palette.common.white}
+          top={0}
+          left={theme.spacing(1)}
+          mt={`-${theme.spacing(2)}`}
+          px={theme.spacing(1)}
         >
-          <Grid item xs={3}>
-            <Typography variant="body1" gutterBottom>
-              Youtube URL:
-            </Typography>
-          </Grid>
-          <Grid item xs={9}>
-            <FormTextField
-              control={control}
-              fieldName="youtubeUrl"
-              rules={{ required: true }}
-              hiddenLabel
-            />
-          </Grid>
-        </Grid>
-        <Grid item xs={3} />
-        <Grid item container xs={9}>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={onShare}
-            fullWidth
+          <Typography
+            variant="h6"
+            gutterBottom
+            color={theme.palette.primary.light}
           >
-            Share
-          </Button>
+            Share a Youtube Movie
+          </Typography>
+        </Box>
+        <Grid container spacing={4} justifyContent="center" alignItems="center">
+          <Grid item container xs={12} justifyContent="center" spacing={3}>
+            <Grid item xs={3} container alignItems="flex-start">
+              <Typography variant="body1" gutterBottom>
+                Youtube URL
+                <Typography
+                  variant="body2"
+                  component="span"
+                  color={theme.palette.primary.light}
+                >
+                  *
+                </Typography>
+              </Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <FormTextField
+                control={control}
+                fieldName="movieId"
+                rules={{ required: true }}
+                hiddenLabel
+              />
+            </Grid>
+
+            <Grid item xs={3} container alignItems="flex-start">
+              <Typography variant="body1" gutterBottom>
+                Title
+                <Typography
+                  variant="body2"
+                  component="span"
+                  color={theme.palette.primary.light}
+                >
+                  *
+                </Typography>
+              </Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <FormTextField
+                control={control}
+                fieldName="movieTitle"
+                rules={{ required: true }}
+                hiddenLabel
+              />
+            </Grid>
+
+            <Grid item xs={3} container alignItems="flex-start">
+              <Typography variant="body1" gutterBottom>
+                Description:
+              </Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <FormTextField
+                control={control}
+                fieldName="movieDescription"
+                rules={{ required: true }}
+                maxRow={4}
+                hiddenLabel
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={3} />
+          <Grid item container xs={9}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={onShareMovie}
+              fullWidth
+              disabled={!isCanShare}
+            >
+              Share
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </Container>
   );
 };
